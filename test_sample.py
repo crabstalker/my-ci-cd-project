20e104b8f291ecbf641fe2bfc3ae07ecd920814a
Leon <Leonboxdale@outlook.com>
2025-05-11 16:31:58 -0400

    Fixed failing test

---
+++ test_sample.py
@@ -0,0 +1,8 @@
+def test_addition():
+    assert 2 + 3 == 5  # This should pass
+
+def test_subtraction():
+    assert 10 - 5 == 5  # This should pass
+
+def test_failure():
+    assert 1 + 1 == 2  # This should fail (just to see error logs)
